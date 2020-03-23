5beffc9f4da5fe24639966a41a3aef990035d718
João Dinis <44101170+JDinis@users.noreply.github.com>
2020-03-23 21:54:40 +0000

    Updates

---
+++ precache-manifest.6e0bf23cb9375d53f49b26512021d6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55c35704d17e21d7627",
+    "url": "/CloudDrive/static/css/main.a58c22f5.chunk.css"
+  },
+  {
+    "revision": "e55c35704d17e21d7627",
+    "url": "/CloudDrive/static/js/main.e55c3570.chunk.js"
+  },
+  {
+    "revision": "c2942edffbdc6f4f7bbd",
+    "url": "/CloudDrive/static/js/runtime~main.c2942edf.js"
+  },
+  {
+    "revision": "caa2d4d1c117074988ae",
+    "url": "/CloudDrive/static/css/2.34740399.chunk.css"
+  },
+  {
+    "revision": "caa2d4d1c117074988ae",
+    "url": "/CloudDrive/static/js/2.caa2d4d1.chunk.js"
+  },
+  {
+    "revision": "3020d085d793a4c8f82b79f8bce50c89",
+    "url": "/CloudDrive/static/media/CloudDriveLogin.3020d085.webp"
+  },
+  {
+    "revision": "f929d9afa071e0d8b855637588248e9c",
+    "url": "/CloudDrive/index.html"
+  }
+];
